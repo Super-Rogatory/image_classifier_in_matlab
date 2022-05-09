532a6682a0f3594e352d58511f25bc7908d1c2dc
super-rogatory <chukwudi10@gmail.com>
2022-05-08 22:43:53 -0700

    feature: program has max testing accuracy of 58.51% with 10 epochs - essential project is finished

--- model.m
+++ model.m
@@ -1,6 +1,5 @@
-% MATH 320 - Building the CNN model
-% Chukwudi Ikem
-% Arsal Khan
+% MATH 320 Project Code:
+% Chukwudi Ikem | Arsal Khan
 
 %% Process data from our data set.
 % Reads every file you *should* have in cifar-10-batches-mat. And returns
@@ -20,31 +19,49 @@ helperCIFAR10Data.download(url,current_directory);
 % filters specifies the number of output features we want
 % kernel_size or filter_size determines the output features
 % recall: each image is 32x32x3
+num_of_channels = 3;
 filters = 32;
 kernel_size = [3 3];
 input_shape = [32 32 3];
 pool_size = [2 2];
-units = 1024;
+units = 512;
 % Inputs 2-d images to a network and normalizes the dataset in one function.
 % This will prove useful when all the layers are applied.
 input_layer = imageInputLayer(input_shape);
 main_layers = [
+    % 2-D convolutional layer that computes dot product of the weights and
+    % the input
     convolution2dLayer(kernel_size, filters, 'Padding','same');
     % activation function to produce an output based on previous input
-    reluLayer;
+    reluLayer();
+    % performs downsampling, computing the maximum value at each "region"
     maxPooling2dLayer(pool_size);
+    reluLayer();
+    convolution2dLayer(kernel_size, filters, 'Padding','same');
+    reluLayer();
+    maxPooling2dLayer(pool_size);
+    convolution2dLayer(kernel_size, 2 * filters, 'Padding','same');
+    reluLayer();
+    maxPooling2dLayer(pool_size);
+];
+final_layers = [
+    % fully connected layer multiplies input by a weight matrix and adds
+    % bias.
     fullyConnectedLayer(units);
-    reluLayer;
-    dropoutLayer(0.5);
+    reluLayer;cd
+    % randomly drops about half of the neurons
+    dropoutLayer(0.1);
     fullyConnectedLayer(10);
+    % activation function - softmax
     softmaxLayer;
+    % computers cross-entropy loss
     classificationLayer;
 ];
-layers = [input_layer; main_layers];
-
+layers = [input_layer; main_layers; final_layers];
+layers(2).Weights = 0.0001 * randn([kernel_size num_of_channels filters]);
 
 %% Train the machine learning model. 
-training_settings = trainingOptions('adam', 'InitialLearnRate', 0.1, 'MaxEpochs', 10, 'MiniBatchSize', 64, 'Verbose', true);
+training_settings = trainingOptions('sgdm', 'InitialLearnRate', 0.001, 'MaxEpochs', 5, 'MiniBatchSize', 64, 'Verbose', true);
 
 cifar10_compiled = trainNetwork(trainingImages, trainingLabels, layers, training_settings);
 %% Test the machine learning model. 
